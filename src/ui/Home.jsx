9e4b91777fc942964792ac3cad708a577d769590
mojvaf <mojgan@zula-tribe.com>
2024-02-16 19:37:45 +0100

    added

--- src/ui/Home.jsx
+++ src/ui/Home.jsx
@@ -4,7 +4,7 @@ function Home() {
       <h1 className="text-center text-xl font-semibold text-yellow-500">
         The best pizza.
         <br />
-        Straight out of the oven, straight to you..
+        Straight out of the oven, straight to you.
       </h1>
     </div>
   );
