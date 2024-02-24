ec75346ab3b136d0c2dcd9e8faadb709a69979c7
mojvaf <mojgan@zula-tribe.com>
2024-02-24 14:34:38 +0100

    changed the buttons

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -5,6 +5,7 @@ import {
   /*redirect,*/ useActionData,
   useNavigate,
 } from 'react-router-dom';
+import Button from '../../ui/Button';
 //import { createOrder } from '../../services/apiRestaurant';
 
 const isValidPhone = (str) =>
@@ -85,16 +86,9 @@ function CreateOrder() {
 
         <div>
           <input type="hidden" name="cart" value={JSON.stringify(cart)} />
-          <button
-            disabled={isSubmitting}
-            className="inline-block rounded-full bg-yellow-400 px-4 py-3 font-semibold uppercase tracking-wide
-             text-stone-800 transition-colors duration-300
-              hover:bg-yellow-300 focus:bg-yellow-300
-               focus:outline-none focus:ring focus:ring-yellow-300 
-               focus:ring-offset-2 disabled:cursor-not-allowed"
-          >
+          <Button disabled={isSubmitting}>
             {isSubmitting ? 'Placing order ...' : 'Order now'}
-          </button>
+          </Button>
         </div>
       </Form>
     </div>
