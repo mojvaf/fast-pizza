f93d07ed9a5951f8291394aee4cc2297608dd409
mojvaf <mojgan@zula-tribe.com>
2024-02-25 00:09:24 +0100

    changed the theme

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -5,8 +5,8 @@ import UserName from '../features/user/UserName';
 export const Header = () => {
   return (
     <header
-      className="flex items-center justify-around border-b-4 border-stone-200
-     bg-yellow-500 px-4 py-3 uppercase sm:px-6"
+      className="font-pizza flex items-center justify-around border-b-4
+     border-stone-200 bg-yellow-500 px-4 py-3 uppercase sm:px-6"
     >
       <Link to="/" className="tracking-[0.5rem]">
         Fast React co.
