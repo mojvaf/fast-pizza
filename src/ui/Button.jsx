1bb593e9ed94da4ae8542fc43757790231f10f64
mojvaf <mojgan@zula-tribe.com>
2024-02-27 15:14:52 +0100

    delete function

--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -1,6 +1,6 @@
 import { Link } from 'react-router-dom';
 
-function Button({ children, disabled, to, type }) {
+function Button({ children, disabled, to, type, onClick }) {
   const base =
     'inline-block text-sm rounded-full bg-yellow-400  font-semibold uppercase tracking-wide text-stone-800 transition-colors duration-300 hover:bg-yellow-300 focus:bg-yellow-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed ';
 
@@ -11,13 +11,19 @@ function Button({ children, disabled, to, type }) {
       'inline-block rounded-full bg-transparent border-2 border-stone-400 font-semibold uppercase tracking-wide text-stone-400 transition-colors duration-300 hover:bg-stone-400 hover:text-stone-100 focus:bg-stone-300 focus:outline-none focus:ring focus:ring-stone-200 focus:ring-offset-2 disabled:cursor-not-allowed px-4 py-2.5 md:px-6 md:py-3.5',
   };
 
-  if (to) {
+  if (to)
     return (
       <Link to={to} className={styles[type]}>
         {children}
       </Link>
     );
-  }
+
+  if (onClick)
+    return (
+      <button onClick={onClick} disabled={disabled} className={styles[type]}>
+        {children}
+      </button>
+    );
 
   return (
     <button disabled={disabled} className={styles[type]}>
